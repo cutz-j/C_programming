27a1176ed4d85c409f5e2443f0896b0435404cba
cutz-j <cutz309@gmail.com>
2018-11-26 23:00:37 +0900

    Update

---
+++ Simpleproject/Hello1.c/Hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // header init
+
+int main(void)
+{
+	/*
+	처음 C를 했다.
+	*/
+	printf("cutz");
+	printf("\nSeoul, Korea");
+	printf("\n(82)-10-9916-0000\n");
+	system("pause");
+	return 0;
+}
