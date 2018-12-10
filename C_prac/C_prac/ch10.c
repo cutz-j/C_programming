16cca8425e6db49f908c95fb9a0f49c1cb13f93b
Hyeonseong Jeon <cutz309@gmail.com>
2018-12-10 16:05:17 +0900

    Update

--- C_prac/C_prac/ch10.c
+++ C_prac/C_prac/ch10.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int sub10(void)
 {
 	//q1();
 	//q2();
---
+++ C_prac/C_prac/ch12.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//int num = 7;
+
+	/*int num = 10;
+	int *pnum = &num;
+	*pnum = 20;
+	printf("%d\n", *pnum);
+
+	num = 7;
+	printf("%d", *pnum);*/
+	//q12_1_1();
+	q12_1_2();
+}
+
+int q12_1_1(void)
+{
+	int num = 10;
+	int * ptr1 = &num;
+	int * ptr2 = ptr1;
+
+	(*ptr1)++;
+	(*ptr2)++;
+	printf("%d \n", num);
+	return 0;
+}
+
+int q12_1_2(void)
+{
+	int num1 = 10, num2 = 20;
+	int *ptr1 = &num1;
+	int *ptr2 = &num2;
+	int temp;
+	(*ptr1) += 10;
+	(*ptr2) -= 10;
+	temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
+	printf("\n%d %d", *ptr1, *ptr2);
+	return 0;
+}
